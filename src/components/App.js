51e4ebb88b6cb265a80faedf590261ff2d7622b4
Brenden Olding <oldingb21@gmail.com>
2023-09-08 01:38:35 -0400

    feat: Added component folder and rough Navbar

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import Navbar from './Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Phase 2 Project Start</h1>
+        <Navbar />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+
+
+export default function Navbar () {
+
+    return (
+        <section>
+            <button>Page 1</button>
+            <button>Page 2</button>
+        </section>
+    )
+}
