593bb58661db827b801f4c6a2c0afa190e7fe073
Brenden Olding <oldingb21@gmail.com>
2023-09-28 23:37:45 -0400

    added image to AddBookForm component

--- src/components/AddBookForm.js
+++ src/components/AddBookForm.js
@@ -25,26 +25,29 @@ const AddBookForm = ({onNewBookSubmit}) => {
     }
 
     return(
-        <form onSubmit={onFormSubmit} className="new-book-form" >
-            <label>Book Title
-                <input name="title" type="text" value={newBook.title} onChange={handleFormChange} required></input>
-            </label>
-            <label>Author
-                <input name="author" type="text" value={newBook.author} onChange={handleFormChange} required></input>
-            </label>
-            <label>Image URL
-                <input name="image" type="text" value={newBook.image} onChange={handleFormChange} required></input>
-            </label>
-            <label>Book List
-                <select name="status" value={newBook.status} onChange={handleFormChange} required>
-                    <option value="" disabled>--Select Book List--</option>
-                    <option value="Finished Reading">Finished Reading</option>
-                    <option value="Actively Reading">Actively Reading</option>
-                    <option value="Reading Wishlist">Reading Wishlist</option>
-                </select>
-            </label>
-            <input type="submit" value="Add Book"></input>
-        </form>
+        <section>
+            <form onSubmit={onFormSubmit} className="new-book-form" >
+                <label>Book Title
+                    <input name="title" type="text" value={newBook.title} onChange={handleFormChange} required></input>
+                </label>
+                <label>Author
+                    <input name="author" type="text" value={newBook.author} onChange={handleFormChange} required></input>
+                </label>
+                <label>Image URL
+                    <input name="image" type="text" value={newBook.image} onChange={handleFormChange} required></input>
+                </label>
+                <label>Book List
+                    <select name="status" value={newBook.status} onChange={handleFormChange} required>
+                        <option value="" disabled>--Select Book List--</option>
+                        <option value="Finished Reading">Finished Reading</option>
+                        <option value="Actively Reading">Actively Reading</option>
+                        <option value="Reading Wishlist">Reading Wishlist</option>
+                    </select>
+                </label>
+                <input type="submit" value="Add Book"></input>
+            </form>
+            <img src='https://wallpaperaccess.com/full/3133075.jpg' alt='Cozy Library Drawing' />
+        </section>
     )
 }
 
